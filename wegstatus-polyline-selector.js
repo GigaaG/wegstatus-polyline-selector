bba5b384166ee0fdb96b0b1211fa069ed215f45b
xhoogland <github@xanland.nl>
2018-05-02 20:16:05 +0200

    Bumped version, added update- and downloadURL for Tampermonkey, and added a hack to fix the button in F(ix)U(I).

--- wegstatus-polyline-selector.js
+++ wegstatus-polyline-selector.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         Wegstatus polyline-selector
 // @namespace    https://wegstatus.nl
-// @version      0.2.0
+// @version      0.2.1
 // @description  Adds a link in the segment-panel to grab the polyline.
 // @author       Xander "Xanland" Hoogland
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor([^\/]?.*)?$/
 // @supportURL   https://github.com/xhoogland/wegstatus-polyline-selector/issues
+// @updateURL    https://raw.githubusercontent.com/xhoogland/wegstatus-polyline-selector/master/wegstatus-polyline-selector.js
+// @downloadURL  https://raw.githubusercontent.com/xhoogland/wegstatus-polyline-selector/master/wegstatus-polyline-selector.js
 // ==/UserScript==
 
-(function() {
+(function () {
     const copyText = 'Click to copy';
     $('head').append('<style type="text/css">#grab-polyline { background-image: url(https://www.wegstatus.nl/favicon-16x16.png); background-repeat: no-repeat; background-size: 20px 20px; background-position-x: 32%; background-position-y: 40%; }</style>');
 
-    function getLatLonOfComponent (component) {
-        const x = component.clone().transform(W.map.getProjection(),'EPSG:4326').toLonLat().lat;
-        const y = component.clone().transform(W.map.getProjection(),'EPSG:4326').toLonLat().lon;
+    function getLatLonOfComponent(component) {
+        const x = component.clone().transform(W.map.getProjection(), 'EPSG:4326').toLonLat().lat;
+        const y = component.clone().transform(W.map.getProjection(), 'EPSG:4326').toLonLat().lon;
 
         return x + ' ' + y;
     }
@@ -22,6 +24,13 @@
     const segmentPanelObserver = new MutationObserver(function (segmentPanel) {
         const selectedItemsCount = W.selectionManager.getSelectedFeatures().length;
         if (selectedItemsCount == 1) {
+            // Easy hack to show the button in F(ix)U(I)
+            const $fuButtons = $('#edit-panel .more-actions');
+            if ($fuButtons.css('display') == 'inline-flex') {
+                $fuButtons.css('display', 'initial');
+                $('head').append('<style type="text/css">#grab-polyline { background-position-x: 10%; }</style>');
+            }
+
             $('#segment-edit-general > div.form-group.more-actions').append('<div class="edit-house-numbers-btn-wrapper"><button class="action-button waze-btn waze-btn-white" id="grab-polyline" title="' + copyText + '">Grab polyline</button><textarea id="grab-polyline-textarea" style="display:none"></textarea></div>');
             $('#grab-polyline').tooltip({ trigger: 'hover' });
             addClickHanderForGrabPolylineButton();
@@ -35,13 +44,13 @@
         segmentPanelObserver.observe(document.querySelector('#edit-panel > div'), { childList: true });
     }, 440);
 
-    function addClickHanderForGrabPolylineButton () {
+    function addClickHanderForGrabPolylineButton() {
         $('#grab-polyline').click(function () {
             const feature = W.selectionManager.getSelectedFeatures()[0];
             const attributes = feature.model.attributes;
             let components = feature.geometry.components;
             let polyline = '';
-            if(attributes.fwdDirection === false && attributes.revDirection === true)
+            if (attributes.fwdDirection === false && attributes.revDirection === true)
                 components.reverse();
             components.forEach(function (component) {
                 polyline = polyline + getLatLonOfComponent(component) + ' ';
