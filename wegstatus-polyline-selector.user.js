3b4eac58db0666422f0fcb95b9a0e73c14dafd84
Sjors <33669464+GigaaG@users.noreply.github.com>
2019-02-28 21:49:05 +0100

    Update wegstatus-polyline-selector.user.js
    Updated after WME update.

--- wegstatus-polyline-selector.user.js
+++ wegstatus-polyline-selector.user.js
@@ -1,23 +1,16 @@
 // ==UserScript==
 // @name         Wegstatus Polyline Selector v2
 // @namespace    https://wegstatus.nl
-// @version      1.0.0
+// @version      1.1.0
 // @description  Adds a link in the segment-panel to grab the polyline.
 // @author       Xander "Xanland" Hoogland & Sjors "GigaaG" Luyckx
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor([^\/]?.*)?$/
-// @supportURL   https://github.com/xhoogland/wegstatus-polyline-selector/issues
+
 // ==/UserScript==
 
 (function () {
     const copyText = 'Click to copy';
     $('head').append('<style type="text/css">#grab-polyline { background-image: url(https://www.wegstatus.nl/favicon-16x16.png); background-repeat: no-repeat; background-size: 20px 20px; background-position-x: 32%; background-position-y: 40%; }</style>');
-
-    function getLatLonOfComponent(component) {
-        const x = component.clone().transform(W.map.getProjection(), 'EPSG:4326').toLonLat().lat;
-        const y = component.clone().transform(W.map.getProjection(), 'EPSG:4326').toLonLat().lon;
-
-        return x + ' ' + y;
-    }
 
     const segmentPanelObserver = new MutationObserver(function (segmentPanel) {
         const selectedItemsCount = W.selectionManager.getSelectedFeatures().length;
@@ -45,21 +38,22 @@
     function addClickHanderForGrabPolylineButton() {
         $('#grab-polyline').click(function () {
             let polyline = '';
-            var countSegments = W.selectionManager._selectedFeatures.length;
-            console.log(countSegments);
+            var countSegments = W.selectionManager.getSelectedFeatures().length;
+            const feature = W.selectionManager.getSelectedFeatures();
+
             for (a = 0 ; a < countSegments ; a++){
-                console.log("Segment: " + a);
-                const feature = W.selectionManager.getSelectedFeatures()[a].model;
-                const attributes = feature.attributes;
-                var components = W.selectionManager._selectedFeatures[a].geometry.components;
-                if (attributes.fwdDirection === false && attributes.revDirection === true)
-                    components.reverse();
-                components.forEach(function (component) {
-                    console.log("Log: Components");
-                    polyline = polyline + getLatLonOfComponent(component) + ' ';
-                });
+                const component = feature[a].geometry.components
+                var points = component.length;
+                for (b = 0 ; b < points ; b++){
+                    var coordinates = component[b].clone().transform(W.map.getProjection(), 'EPSG:4326');
+                    var x = coordinates.x
+                    var y = coordinates.y
+                    var latlon = ''+ y +' '+ x +' ';
+                    polyline += latlon
+                    console.log(latlon);
+                }
             }
-            console.log(polyline);
+
             $('#grab-polyline-textarea').val(polyline.trim());
             const copyText = document.querySelector("#grab-polyline-textarea");
             $('#grab-polyline-textarea').show();
