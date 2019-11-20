d5808ab095739e273fba3db1e79a4e96b21d3c97
Sjors <33669464+GigaaG@users.noreply.github.com>
2019-11-20 12:16:14 +0100

    Update wegstatus-polyline-selector.user.js

--- wegstatus-polyline-selector.user.js
+++ wegstatus-polyline-selector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wegstatus Polyline Selector
 // @namespace    https://wegstatus.nl
-// @version      2019.05.8.13
+// @version      2019.11.20.01
 // @description  Adds a link in the segment-panel to grab the polyline.
 // @author       Xander "Xanland" Hoogland & Sjors "GigaaG" Luyckx
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor([^\/]?.*)?$/
